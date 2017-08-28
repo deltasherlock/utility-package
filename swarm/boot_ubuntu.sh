3f898db766f0de0e47b56358b1703b61ba0a3961
Anthony Byrne <anthony@abyrne.me>
2017-08-28 14:18:25 -0400

    Bug fix on boot script

--- swarm/boot_ubuntu.sh
+++ swarm/boot_ubuntu.sh
@@ -2,18 +2,19 @@
 # DeltaSherlock Swarm Member Bootstrap Script
 # For Ubuntu/Debian
 # Run this immediately after boot
+export DEBIAN_FRONTEND=noninteractive
 
 # Fix any broken packages that may have arisen from a previous reboot
 rm /var/lib/apt/lists/lock
 rm /var/cache/apt/archives/lock
-apt-get update --fix-missing
+apt-get update -yq --fix-missing
 dpkg --configure -a
-apt-get install -f
+apt-get install -yq -f
 
 cd /root/utility-package/swarm
 
 # Remove any lingering packages from the list
-apt-get remove --purge $(grep -vE "^\s*#" repo_packages_ubuntu.lst  | tr "\n" " ")
+apt-get remove -yq --purge $(grep -vE "^\s*#" repo_packages_ubuntu.lst  | tr "\n" " ")
 
 # Update source code
 git pull
