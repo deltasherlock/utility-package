06efe7860607f4ab10da28c99ed268c47c48837a
Anthony Byrne <anthony@abyrne.me>
2017-09-21 16:49:45 -0400

    typo fix

--- swarm/boot_centos.sh
+++ swarm/boot_centos.sh
@@ -2,7 +2,6 @@
 # DeltaSherlock Swarm Member Bootstrap Script
 # For CentOS 7
 # Run this immediately after boot
-export DEBIAN_FRONTEND=noninteractive
 
 # Fix any broken packages that may have arisen from a previous reboot
 yum-complete-transaction -y
@@ -11,7 +10,7 @@ package-cleanup --cleandupes -y
 cd /root/utility-package/swarm
 
 # Remove any lingering packages from the list
-yum remove $(grep -vE "^\s*#" repo_packages_centos.lst  | tr "\n" " ")
+yum remove -y $(grep -vE "^\s*#" repo_packages_centos.lst  | tr "\n" " ")
 
 # Update source code
 git pull
